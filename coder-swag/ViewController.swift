b5c9304646daff267560e896d2b10940ed47246a
Mubarak Almahmeed <mubarakalmahmeed@mubaraks-macbook-pro.local>
2019-02-01 13:51:42 +0300

    Initial Commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Mubarak Almahmeed on 2/1/19.
+//  Copyright © 2019 Mubarak Almahmeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
